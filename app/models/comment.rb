743bba180aa322929e4cb63646883bcba9a8e59b
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-05 17:42:48 +0100

    Add update_comments_counter to Comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def update_comments_counter(number)
+    post.update(CommentsCounter: number)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def update_likes_counter(number)
+    post.update(LikesCounter: number)
+  end
 end
