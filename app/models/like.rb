095c5518ce44acd2eb1ee24608f004ed074f2f40
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-06 12:28:56 +0100

    Adjust class_name for author.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :post
-  belongs_to :user
+  belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
 
   def update_likes_counter(number)
     post.update(LikesCounter: number)
