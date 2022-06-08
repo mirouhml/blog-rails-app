367972bac72030e24ce176574f09515ebd137d1b
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-08 19:10:19 +0100

    Add new and create to posts_controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    user = current_user
+    @post.author = user
+
+    render :new unless @post.save
+
+    redirect_to user_posts_path(user, @post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
