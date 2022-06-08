4225e0c907c87b0b72077ede21eca575970437b5
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-08 23:49:52 +0100

    Fix validation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :CommentsCounter, presence: true, numericality { greater_than_or_equal_to: 0 }
-  validates :LikesCounter, presence: true, numericality { greater_than_or_equal_to: 0 }
+  validates :CommentsCounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :LikesCounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
 
   def update_posts_counter(number)
