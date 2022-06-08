70cfa15665da121bcfa34ae5fee1892fe4037191
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-08 19:10:43 +0100

    Implement likes_controller.

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    user = current_user
+    liked = Like.where(author: user, post: post)
+
+    return if liked.present?
+
+    like = Like.create(author: user, post: post)
+    return unless like.save
+
+    redirect_to user_post_path(post.author, post)
+  end
+end
