45cc94afb4e38a6701a233fbc3d9c7e55d85bf12
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-09 08:46:50 +0100

    Add validators.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-
+ 
+  validates :post, presence: true
+  validates :author, presence: true
+  
   def update_likes_counter(number)
     post.update(LikesCounter: number)
   end
