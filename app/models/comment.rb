d503539126e3e0132f5ee93fa6fead0d009b266e
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-09 08:46:35 +0100

    Add validators.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :author, presence: true
+  validates :post, presence: true
+  validates :text, presence: true
+  
   def update_comments_counter(number)
     post.update(CommentsCounter: number)
   end
