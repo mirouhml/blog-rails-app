331f173efd1f257674280c6ab2a8195280599627
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-09 08:48:03 +0100

    Add unit tests for like model.

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Like, type: :model do
+  subject {
+    user = User.create(name: 'John', photo: 'Random photo', bio: 'Random bio', PostsCounter: 0)
+    post = Post.create(title: 'title', text: 'eRandom text', CommentsCounter: 0, LikesCounter: 0, author_id: user.id) 
+    Like.new(author_id: user.id, post_id: post.id)
+  }
+
+  before { subject.save }
+
+  it 'Author should be present' do
+    subject.author = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Post should be present' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'update_likes_counter should update the counter to 2' do
+    subject.update_likes_counter(2)
+    expect(subject.post.LikesCounter).to eq 2
+  end
+end
