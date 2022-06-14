eca8ca6eb0d13cc2d51a6e00caa9f65016b84442
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-14 10:22:48 +0100

    Increment posts counter when a post is saved.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     return unless User.exists?(params[:user_id])
 
-    @user = @user = User.includes(posts: %i[comments likes]).find_by(id: params[:user_id])
+    @user = @user = User.includes(posts: %i[comments]).find_by(id: params[:user_id])
     @posts = @user.posts
   end
 
@@ -18,11 +18,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
     user = current_user
+    @post = Post.new(post_params)
     @post.author = user
-
-    if @post.user
+    if @post.save
+      user.PostsCounter += 1
+      user.save
       redirect_to user_posts_path(user, @post), notice: 'Post was successfully created!'
     else
       render :new
