7acee93be3894c3621a29910b389e4ed49a2e212
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-15 09:42:16 +0100

    Add destroy for like.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,4 +17,19 @@ class LikesController < ApplicationController
       end
     end
   end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    user = current_user
+    like = Like.where(author: user, post: post)
+
+    if like.present?
+      like.destroy
+      post.LikesCounter -= 1
+      post.save
+      redirect_to user_post_path(post.author, post), notice: 'Post successfully unliked'
+    else
+      flash[:alert] = 'Could not unlike the post, please try again later.'
+    end
+  end
 end
