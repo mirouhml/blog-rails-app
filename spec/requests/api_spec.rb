86a3eb89d4d2bc7415741946debb6b45ba8f5959
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-17 20:39:10 +0100

    Fix linters.

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -1,7 +1,6 @@
 require 'swagger_helper'
 
 RSpec.describe 'api', type: :request do
-
   path '/users/{user_id}/posts_list' do
     # You'll want to customize the parameter types...
     parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
