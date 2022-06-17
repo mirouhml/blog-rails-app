80600b2a253af77770b609843bbfaccc6f5e3e59
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-17 03:40:56 +0100

    Add routes for all api endpoints.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[index new create destroy]
+      resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[create destroy]
     end
   end
-
+  get 'users/:user_id/posts_list', to: 'api#list_all_user_posts', as: :list_all_user_posts
+  get 'users/:user_id/posts/:post_id/comments_list', to: 'api#list_all_user_post_comments', as: :list_all_user_post_comments
+  post 'users/:user_id/posts/:post_id/add_comment', to: 'api#add_comment', as: :add_comment
   root to: 'users#index'
 end
